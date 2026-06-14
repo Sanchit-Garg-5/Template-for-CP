1d1593ec4c4a0d4f8094d7e65d8e00305dc5511b
Sanchit-Garg-5 <sanchitgargsoldier@gmail.com>
2026-06-14 18:10:58 +0530

    Add files via upload

--- template.cpp
+++ template.cpp
@@ -1,33 +1,50 @@
 #include <bits/stdc++.h>
 using namespace std;
-// --- Companion macros required for this debugger to compile ---
+// --- Companion macros required for this debugger ---
 typedef long long ll;
 #define f first
 #define s second
-
-// --- The Debugger Block ---
+// --- Master Debugger Block ---
 #define debug(x) _print(x); cerr << endl;
-
 void _print(ll t) {cerr << t;}
 void _print(int t) {cerr << t;}
 void _print(string t) {cerr << t;}
 void _print(char t) {cerr << t;}
 void _print(double t) {cerr << t;}
-
 template <class T, class V> void _print(pair <T, V> p);
 template <class T> void _print(vector <T> v);
 template <class T> void _print(set <T> v);
 template <class T> void _print(multiset <T> v);
 template <class T, class V> void _print(map <T, V> v);
+template <class... Args> void _print(unordered_set <Args...> v);
+template <class... Args> void _print(unordered_multiset <Args...> v);
+template <class... Args> void _print(unordered_map <Args...> v);
+template <class... Args> void _print(unordered_multimap <Args...> v);
+template <class... Args> void _print(list <Args...> v);
+template <class... Args> void _print(forward_list <Args...> v);
+template <class... Args> void _print(stack <Args...> v);
+template <class... Args> void _print(queue <Args...> v);
+template <class... Args> void _print(priority_queue <Args...> v);
 
 template <class T, class V> void _print(pair <T, V> p) {cerr << "{"; _print(p.f); cerr << ","; _print(p.s); cerr << "}";}
 template <class T> void _print(vector <T> v) {cerr << "[ "; for (T i : v) {_print(i); cerr << " ";} cerr << "]";}
 template <class T> void _print(set <T> v) {cerr << "[ "; for (T i : v) {_print(i); cerr << " ";} cerr << "]";}
 template <class T> void _print(multiset <T> v) {cerr << "[ "; for (T i : v) {_print(i); cerr << " ";} cerr << "]";}
 template <class T, class V> void _print(map <T, V> v) {cerr << "[ "; for (auto i : v) {_print(i); cerr << " ";} cerr << "]";}
+template <class... Args> void _print(unordered_set <Args...> v) {cerr << "[ "; for (auto i : v) {_print(i); cerr << " ";} cerr << "]";}
+template <class... Args> void _print(unordered_multiset <Args...> v) {cerr << "[ "; for (auto i : v) {_print(i); cerr << " ";} cerr << "]";}
+template <class... Args> void _print(unordered_map <Args...> v) {cerr << "[ "; for (auto i : v) {_print(i); cerr << " ";} cerr << "]";}
+template <class... Args> void _print(unordered_multimap <Args...> v) {cerr << "[ "; for (auto i : v) {_print(i); cerr << " ";} cerr << "]";}
+template <class... Args> void _print(list <Args...> v) {cerr << "[ "; for (auto i : v) {_print(i); cerr << " ";} cerr << "]";}
+template <class... Args> void _print(forward_list <Args...> v) {cerr << "[ "; for (auto i : v) {_print(i); cerr << " ";} cerr << "]";}
+template <class... Args> void _print(stack <Args...> v) {cerr << "[ "; while (!v.empty()) {_print(v.top()); cerr << " "; v.pop();} cerr << "]";}
+template <class... Args> void _print(queue <Args...> v) {cerr << "[ "; while (!v.empty()) {_print(v.front()); cerr << " "; v.pop();} cerr << "]";}
+template <class... Args> void _print(priority_queue <Args...> v) {cerr << "[ "; while (!v.empty()) {_print(v.top()); cerr << " "; v.pop();} cerr << "]";}
 
 int main(){
-    cin.tie(nullptr);cout.tie(nullptr); ios::sync_with_stdio(false);
-    vector<pair<long long int,string>>me; me={{2,"hi"},{3,"me"}};
+    cin.tie(nullptr); cout.tie(nullptr); ios::sync_with_stdio(false);
+    vector<long long int>me;
+    me.push_back(2);
+    me.push_back(6);
     debug(me);
 }
