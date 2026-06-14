aa8543944493cc2380cce76ee4744c02b50d683f
Sanchit-Garg-5 <sanchitgargsoldier@gmail.com>
2026-06-14 17:58:11 +0530

    Add files via upload

---
+++ template.cpp
@@ -0,0 +1,33 @@
+#include <bits/stdc++.h>
+using namespace std;
+// --- Companion macros required for this debugger to compile ---
+typedef long long ll;
+#define f first
+#define s second
+
+// --- The Debugger Block ---
+#define debug(x) _print(x); cerr << endl;
+
+void _print(ll t) {cerr << t;}
+void _print(int t) {cerr << t;}
+void _print(string t) {cerr << t;}
+void _print(char t) {cerr << t;}
+void _print(double t) {cerr << t;}
+
+template <class T, class V> void _print(pair <T, V> p);
+template <class T> void _print(vector <T> v);
+template <class T> void _print(set <T> v);
+template <class T> void _print(multiset <T> v);
+template <class T, class V> void _print(map <T, V> v);
+
+template <class T, class V> void _print(pair <T, V> p) {cerr << "{"; _print(p.f); cerr << ","; _print(p.s); cerr << "}";}
+template <class T> void _print(vector <T> v) {cerr << "[ "; for (T i : v) {_print(i); cerr << " ";} cerr << "]";}
+template <class T> void _print(set <T> v) {cerr << "[ "; for (T i : v) {_print(i); cerr << " ";} cerr << "]";}
+template <class T> void _print(multiset <T> v) {cerr << "[ "; for (T i : v) {_print(i); cerr << " ";} cerr << "]";}
+template <class T, class V> void _print(map <T, V> v) {cerr << "[ "; for (auto i : v) {_print(i); cerr << " ";} cerr << "]";}
+
+int main(){
+    cin.tie(nullptr);cout.tie(nullptr); ios::sync_with_stdio(false);
+    vector<pair<long long int,string>>me; me={{2,"hi"},{3,"me"}};
+    debug(me);
+}
